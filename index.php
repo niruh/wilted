2bbf5f0f182bb77a26612d5fd9098ae6deb45639
E <erinmck@icloud.com>
2024-12-06 21:57:57 +1100

    remove php

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php include("over.php");?>
-
-index
-
-<?php include("under.php");?>
---
+++ php/index.php
@@ -0,0 +1,19 @@
+<?php include("over.php");?>
+
+
+  <div class="content">
+  <div class="mntwrapper">
+
+
+    <div class="desc">
+      <h1>wilted</h1>
+    </div>
+
+    <div class="blurb">
+     In lectus faucibus suscipit posuere dapibus mi justo, vulputate, lacus. Imperdiet faucibus imperdiet lacus consectetur sit dui aenean tempor, faucibus. Quisque ante vulputate tincidunt, dui lorem quam est porta lorem. Enim sapien posuere nulla dapibus pulvinar at feugiat morbi, risus. Laoreet tincidunt euismod faucibus convallis, convallis, pharetra porttitor nulla in.  
+    </div>
+
+  </div>
+  </div>
+
+<?php include("under.php");?>
