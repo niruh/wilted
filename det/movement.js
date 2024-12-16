a74435943b494d861d48eb0ac2e2aa0604d2e3ca
E <erinmck@icloud.com>
2024-12-16 14:18:02 +1100

    any changes here

--- det/movement.js
+++ det/movement.js
@@ -1,7 +1,8 @@
 const button = document.getElementById("move-two");
 const sectionTwo = document.getElementById("two");
 
+function movePage() {
 button.addEventListener("click", () => {
   sectionTwo.scrollIntoView({ behavior: "instant", block: "end" });
 });
-
+}
